702ce35f56df29f18fdaaad5c54edd0c2c023a7e
carsoninho <james.87.carson@gmail.com>
2024-06-06 19:15:44 +0200

    Added screen update functionality for numbers and operators
    Will need to stop it from flowing over the side.

--- script.js
+++ script.js
@@ -46,72 +46,58 @@ function handleOperatorClick(operatorEntry) {
 
 // Numbers:
 
-const zeroButton = document.getElementById("zero_Button");
-zeroButton.addEventListener("click", () =>
-    handleNumberClick('0'));
-const oneButton = document.getElementById("one_Button");
-oneButton.addEventListener("click", () =>
-    handleNumberClick('1'));
-const twoButton = document.getElementById("two_Button");
-twoButton.addEventListener("click", () =>
-    handleNumberClick('2'));
-const threeButton = document.getElementById("three_Button");
-threeButton.addEventListener("click", () =>
-    handleNumberClick('3'));
-const fourButton = document.getElementById("four_Button");
-fourButton.addEventListener("click", () =>
-    handleNumberClick('4'));
-const fiveButton = document.getElementById("five_Button");
-fiveButton.addEventListener("click", () =>
-    handleNumberClick('5'));
-const sixButton = document.getElementById("six_Button");
-sixButton.addEventListener("click", () =>
-    handleNumberClick('6'));
-const sevenButton = document.getElementById("seven_Button");
-sevenButton.addEventListener("click", () =>
-    handleNumberClick('7'));
-const eightButton = document.getElementById("eight_Button");
-eightButton.addEventListener("click", () =>
-    handleNumberClick('8'));
-const nineButton = document.getElementById("nine_Button");
-nineButton.addEventListener("click", () =>
-    handleNumberClick('9'));
+const zeroButton = document.getElementById("zero_button");
+    zeroButton.addEventListener("click", () => handleNumberClick('0'));
+const oneButton = document.getElementById("one_button");
+    oneButton.addEventListener("click", () => handleNumberClick('1'));
+const twoButton = document.getElementById("two_button");
+    twoButton.addEventListener("click", () => handleNumberClick('2'));
+const threeButton = document.getElementById("three_button");
+    threeButton.addEventListener("click", () => handleNumberClick('3'));
+const fourButton = document.getElementById("four_button");
+    fourButton.addEventListener("click", () => handleNumberClick('4'));
+const fiveButton = document.getElementById("five_button");
+    fiveButton.addEventListener("click", () => handleNumberClick('5'));
+const sixButton = document.getElementById("six_button");
+    sixButton.addEventListener("click", () => handleNumberClick('6'));
+const sevenButton = document.getElementById("seven_button");
+    sevenButton.addEventListener("click", () => handleNumberClick('7'));
+const eightButton = document.getElementById("eight_button");
+    eightButton.addEventListener("click", () => handleNumberClick('8'));
+const nineButton = document.getElementById("nine_button");
+    nineButton.addEventListener("click", () => handleNumberClick('9'));
 
 
 // Operators:
 
-const addButton = document.getElementById("add_Button");
-addButton.addEventListener("click", () =>
-    handleOperatorClick('+'));
-const subtractButton = document.getElementById("subtract_Button");
-subtractButton.addEventListener("click", () =>
-    handleOperatorClick('-'));
-const multiplyButton = document.getElementById("multiply_Button");
-multiplyButton.addEventListener("click", () =>
-    handleOperatorClick('*'));
-const divideButton = document.getElementById("divide_Button");
-divideButton.addEventListener("click", () =>
-    handleOperatorClick('/'));
+const addButton = document.getElementById("add_button");
+    addButton.addEventListener("click", () => handleOperatorClick('+'));
+const subtractButton = document.getElementById("subtract_button");
+    subtractButton.addEventListener("click", () => handleOperatorClick('-'));
+const multiplyButton = document.getElementById("multiply_button");
+    multiplyButton.addEventListener("click", () => handleOperatorClick('*'));
+const divideButton = document.getElementById("divide_button");
+    divideButton.addEventListener("click", () => handleOperatorClick('/'));
 
 // Other buttons:
 
-const powerButton = document.getElementById("power_Button");
+const powerButton = document.getElementById("power_button");
 powerButton.addEventListener("click", function () {
     // Add function here!
 });
-const acButton = document.getElementById("ac_Button");
+const acButton = document.getElementById("ac_button");
 acButton.addEventListener("click", function () {
     // Add function here!
 });
-const cButton = document.getElementById("c_Button");
+const cButton = document.getElementById("c_button");
 cButton.addEventListener("click", function () {
     // Add function here!
 });
-const pointButton = document.getElementById("point_Button");
+const pointButton = document.getElementById("point_button");
 pointButton.addEventListener("click", function () {
     // Add function here!
 });
-const equalsButton = document.getElementById("equals_Button");
+const equalsButton = document.getElementById("equals_button");
 equalsButton.addEventListener("click", function () {
     // Add function here!
 });
