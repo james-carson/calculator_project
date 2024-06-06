c8fd6863debb61f8f15786333446383297affc57
carsoninho <james.87.carson@gmail.com>
2024-06-06 16:42:55 +0200

    Alter basic layout and create additional functions.
    Event Listeners need to be added next.

--- script.js
+++ script.js
@@ -2,6 +2,17 @@ let firstNumber,
     secondNumber,
     operator;
 
+// Need to add event listeners next:
+
+// // Select the equals button div
+// const equalsButton = document.getElementById("equalsButton");
+
+// // Add event listener for click event
+// equalsButton.addEventListener("click", function() {
+//     // Call a function or execute logic when the equals button is clicked
+// });
+
+
     const add = function(a, b) {
         return (a + b)
     };
@@ -19,7 +30,27 @@ let firstNumber,
     };
 
     const operate = function (a, b, operator){
+        if (operator === addButton){
+            return add (a, b);
+        } else if (operator === subtractButton){
+            return subtract (a, b);
+        } else if (operator === multiplyButton){
+            return multiply (a, b);
+        } else if (operator === divideButton){
+            return divide (a, b);
+        } else{
+            // Display an error on the screen!
+        }
+    };
+
+    const clearLast = function() {
 
     };
 
-    
+    const clearAll = function() {
+
+    };
+
+    const togglePower = function(){
+
+    }
