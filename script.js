d81b00f631c7798dc37ba69b4a636cd698d943f0
carsoninho <james.87.carson@gmail.com>
2024-06-07 11:40:11 +0200

    Add memory variable and fix clear functions

--- script.js
+++ script.js
@@ -2,19 +2,44 @@
 
 let firstNumber = '',
     secondNumber = '',
-    operator = '';
+    operator = '',
+    firstNumberTop = '',
+    operatorTop = '',
+    secondNumberTop = '';
+    memory = '';
 
 // --Allowing the screen to be updated:
 const lowerScreen = document.getElementById("screen_bottom");
 const upperScreen = document.getElementById("screen_top");
 
 function updateScreen() {
+    upperScreen.textContent = '';
+    lowerScreen.textContent = firstNumber + operator + secondNumber;
+}
+function updateLowerScreen() {
     lowerScreen.textContent = firstNumber + operator + secondNumber;
 }
 function updateScreenAnswer() {
-    upperScreen.textContent = firstNumber + operator + secondNumber;
-    lowerScreen.textContent = operate(firstNumber, operator, secondNumber);
+    firstNumberTop = firstNumber;
+    operatorTop = operator;
+    secondNumberTop = secondNumber;
+    upperScreen.textContent = firstNumberTop + operatorTop + secondNumberTop;
+    operate(firstNumber, operator, secondNumber);
+    lowerScreen.textContent = memory;
 }
+function displayError() {
+    firstNumber = '';
+        secondNumber = '';
+        operator = '';
+        firstNumberTop = '';
+        operatorTop = '';
+        secondNumberTop = '';
+    upperScreen.textContent = '';
+    lowerScreen.textContent = 'ERROR!';
+}
+
+
+
 // ***** Need to stop it from flowing over the edge of the screen! ****
 
 // --Click handlers to update variables:
@@ -34,17 +59,17 @@ function handleNumberClick(number) {
             secondNumber += number;
         }
     } else {
-        // Update screen to read "ERROR" later!
+        displayError();
     }
     updateScreen()
 }
 
 // Click handler to update operator variable on operator clicks:
 function handleOperatorClick(operatorEntry) {
-    if (firstNumber && !firstNumber == '0' && !operator) {
+    if (firstNumber && firstNumber !== '0' && !operator) {
         operator = operatorEntry
     } else {
-        // Update screen to read "ERROR" later! 
+        displayError();
     }
     updateScreen()
 }
@@ -54,37 +79,37 @@ function handleOperatorClick(operatorEntry) {
 // Numbers:
 
 const zeroButton = document.getElementById("zero_button");
-    zeroButton.addEventListener("click", () => handleNumberClick('0'));
+zeroButton.addEventListener("click", () => handleNumberClick('0'));
 const oneButton = document.getElementById("one_button");
-    oneButton.addEventListener("click", () => handleNumberClick('1'));
+oneButton.addEventListener("click", () => handleNumberClick('1'));
 const twoButton = document.getElementById("two_button");
-    twoButton.addEventListener("click", () => handleNumberClick('2'));
+twoButton.addEventListener("click", () => handleNumberClick('2'));
 const threeButton = document.getElementById("three_button");
-    threeButton.addEventListener("click", () => handleNumberClick('3'));
+threeButton.addEventListener("click", () => handleNumberClick('3'));
 const fourButton = document.getElementById("four_button");
-    fourButton.addEventListener("click", () => handleNumberClick('4'));
+fourButton.addEventListener("click", () => handleNumberClick('4'));
 const fiveButton = document.getElementById("five_button");
-    fiveButton.addEventListener("click", () => handleNumberClick('5'));
+fiveButton.addEventListener("click", () => handleNumberClick('5'));
 const sixButton = document.getElementById("six_button");
-    sixButton.addEventListener("click", () => handleNumberClick('6'));
+sixButton.addEventListener("click", () => handleNumberClick('6'));
 const sevenButton = document.getElementById("seven_button");
-    sevenButton.addEventListener("click", () => handleNumberClick('7'));
+sevenButton.addEventListener("click", () => handleNumberClick('7'));
 const eightButton = document.getElementById("eight_button");
-    eightButton.addEventListener("click", () => handleNumberClick('8'));
+eightButton.addEventListener("click", () => handleNumberClick('8'));
 const nineButton = document.getElementById("nine_button");
-    nineButton.addEventListener("click", () => handleNumberClick('9'));
+nineButton.addEventListener("click", () => handleNumberClick('9'));
 
 
 // Operators:
 
 const addButton = document.getElementById("add_button");
-    addButton.addEventListener("click", () => handleOperatorClick('+'));
+addButton.addEventListener("click", () => handleOperatorClick('+'));
 const subtractButton = document.getElementById("subtract_button");
-    subtractButton.addEventListener("click", () => handleOperatorClick('-'));
+subtractButton.addEventListener("click", () => handleOperatorClick('-'));
 const multiplyButton = document.getElementById("multiply_button");
-    multiplyButton.addEventListener("click", () => handleOperatorClick('*'));
+multiplyButton.addEventListener("click", () => handleOperatorClick('*'));
 const divideButton = document.getElementById("divide_button");
-    divideButton.addEventListener("click", () => handleOperatorClick('/'));
+divideButton.addEventListener("click", () => handleOperatorClick('/'));
 
 // Other buttons:
 
@@ -94,11 +119,11 @@ powerButton.addEventListener("click", function () {
 });
 const acButton = document.getElementById("ac_button");
 acButton.addEventListener("click", function () {
-    // Add function here!
+    clearAll();
 });
 const cButton = document.getElementById("c_button");
 cButton.addEventListener("click", function () {
-    // Add function here!
+    clearLast()
 });
 const pointButton = document.getElementById("point_button");
 pointButton.addEventListener("click", function () {
@@ -106,49 +131,61 @@ pointButton.addEventListener("click", function () {
 });
 const equalsButton = document.getElementById("equals_button");
 equalsButton.addEventListener("click", function () {
-    updateScreenAnswer()
+    updateScreenAnswer();
 });
 
 
 // Operator functions:
 
 const add = function (firstNumber, secondNumber) {
-    return (parseInt(firstNumber)) + (parseInt(secondNumber))
+    return (parseInt(firstNumber)) + (parseInt(secondNumber));
 };
-
 const subtract = function (firstNumber, secondNumber) {
-    return (parseInt(firstNumber)) - (parseInt(secondNumber))
+    return (parseInt(firstNumber)) - (parseInt(secondNumber));
 };
-
 const multiply = function (firstNumber, secondNumber) {
-    return (parseInt(firstNumber)) * (parseInt(secondNumber))
+    return (parseInt(firstNumber)) * (parseInt(secondNumber));
 };
-
 const divide = function (firstNumber, secondNumber) {
-    return (parseInt(firstNumber)) / (parseInt(secondNumber))
+    return (parseInt(firstNumber)) / (parseInt(secondNumber));
 };
 
 const operate = function (firstNumber, operator, secondNumber) {
     if (operator === '+') {
-        return add(firstNumber, secondNumber);
+        result =  add(firstNumber, secondNumber);
     } else if (operator === '-') {
-        return subtract(firstNumber, secondNumber);
+        result =  subtract(firstNumber, secondNumber);
     } else if (operator === '*') {
-        return multiply(firstNumber, secondNumber);
+        result =  multiply(firstNumber, secondNumber);
     } else if (operator === '/') {
-        return divide(firstNumber, secondNumber);
+        result =  divide(firstNumber, secondNumber);
     } else {
-        // Display an error on the screen!
+        displayError();
+        return;
     }
-    
+    memory = result;
+    return result;
 };
 
 const clearLast = function () {
-
+    if (firstNumber && operator && secondNumber) {
+        secondNumber = secondNumber.slice(0, -1);
+    } else if (firstNumber && operator && !secondNumber) {
+        operator = '';
+    } else if (firstNumber && !operator && !secondNumber) {
+        firstNumber = firstNumber.slice(0, -1);
+    }
+    updateLowerScreen();
 };
 
 const clearAll = function () {
-
+    firstNumber = '';
+    operator = '';
+    secondNumber = '';
+    firstNumberTop = '',
+        operatorTop = '',
+        secondNumberTop = '';
+    updateScreen();
 };
 
 const togglePower = function () {
