9befe223866aaece329dec6ddbfb16f918ee62c3
carsoninho <james.87.carson@gmail.com>
2024-06-09 15:35:38 +0200

    Add lengthCheck to limit input
    Input is limited to 12 characters

--- script.js
+++ script.js
@@ -79,6 +79,14 @@ const deleteAll = function () {
     memory = '';
 }
 
+const lengthCheck = function () {
+    if (lowerScreen.textContent.length < 12) {
+        return true
+    } else {
+        return false
+    }
+}
+
 const togglePower = function () {
 
 }
@@ -132,50 +140,55 @@ function displayError() {
 
 // Click handler to update variables on number clicks:
 function handleNumberClick(number) {
-    if (memory && operator) {
-        secondNumber += number;
-    } else if (!memory) {
-        if (!operator && !secondNumber) {
-            if (firstNumber === '0') {
-                firstNumber = number;
-            } else {
-                firstNumber += number;
-            }
-        } else if (firstNumber && operator) {
-            if (secondNumber === '0') {
-                secondNumber = number;
-            } else {
-                secondNumber += number;
-            }
-        }
-    } else {
-        displayError();
-    }
-    updateLowerScreen();
-}
-
-// Click handler to update operator variable on operator clicks:
-function handleOperatorClick(operatorEntry) {
-    if (!operator) {
-        if (memory) {
-            firstNumber = memory
-            operator = operatorEntry;
-            secondNumber = '';
-            updateLowerScreen()
+    if (lengthCheck()) {
+        if (memory && operator) {
+            secondNumber += number;
         } else if (!memory) {
-            operator = operatorEntry;
-            updateScreen()
+            if (!operator && !secondNumber) {
+                if (firstNumber === '0') {
+                    firstNumber = number;
+                } else {
+                    firstNumber += number;
+                }
+            } else if (firstNumber && operator) {
+                if (secondNumber === '0') {
+                    secondNumber = number;
+                } else {
+                    secondNumber += number;
+                }
+            }
         } else {
             displayError();
         }
-    } else if (operator){
-        operator = operatorEntry;
         updateLowerScreen();
     }
 }
 
-    // Click handler to handle decimal point button click:
-    function handleDecimalPoint() {
+// Click handler to update operator variable on operator clicks:
+function handleOperatorClick(operatorEntry) {
+    if (lengthCheck()) {
+        if (!operator) {
+            if (memory) {
+                firstNumber = memory
+                operator = operatorEntry;
+                secondNumber = '';
+                updateLowerScreen()
+            } else if (!memory) {
+                operator = operatorEntry;
+                updateScreen()
+            } else {
+                displayError();
+            }
+        } else if (operator) {
+            operator = operatorEntry;
+            updateLowerScreen();
+        }
+    }
+}
+
+// Click handler to handle decimal point button click:
+function handleDecimalPoint() {
+    if (lengthCheck()) {
         if (!operator) {
             if (!firstNumber && memory) {
                 if (!memory.includes(".")) {
@@ -195,63 +208,64 @@ function handleOperatorClick(operatorEntry) {
         }
         updateLowerScreen();
     }
+}
 
-    // --Event listeners to listen for a button click:
+// --Event listeners to listen for a button click:
 
-    // Numbers:
+// Numbers:
 
-    const zeroButton = document.getElementById("zero_button");
-    zeroButton.addEventListener("click", () => handleNumberClick('0'));
-    const oneButton = document.getElementById("one_button");
-    oneButton.addEventListener("click", () => handleNumberClick('1'));
-    const twoButton = document.getElementById("two_button");
-    twoButton.addEventListener("click", () => handleNumberClick('2'));
-    const threeButton = document.getElementById("three_button");
-    threeButton.addEventListener("click", () => handleNumberClick('3'));
-    const fourButton = document.getElementById("four_button");
-    fourButton.addEventListener("click", () => handleNumberClick('4'));
-    const fiveButton = document.getElementById("five_button");
-    fiveButton.addEventListener("click", () => handleNumberClick('5'));
-    const sixButton = document.getElementById("six_button");
-    sixButton.addEventListener("click", () => handleNumberClick('6'));
-    const sevenButton = document.getElementById("seven_button");
-    sevenButton.addEventListener("click", () => handleNumberClick('7'));
-    const eightButton = document.getElementById("eight_button");
-    eightButton.addEventListener("click", () => handleNumberClick('8'));
-    const nineButton = document.getElementById("nine_button");
-    nineButton.addEventListener("click", () => handleNumberClick('9'));
+const zeroButton = document.getElementById("zero_button");
+zeroButton.addEventListener("click", () => handleNumberClick('0'));
+const oneButton = document.getElementById("one_button");
+oneButton.addEventListener("click", () => handleNumberClick('1'));
+const twoButton = document.getElementById("two_button");
+twoButton.addEventListener("click", () => handleNumberClick('2'));
+const threeButton = document.getElementById("three_button");
+threeButton.addEventListener("click", () => handleNumberClick('3'));
+const fourButton = document.getElementById("four_button");
+fourButton.addEventListener("click", () => handleNumberClick('4'));
+const fiveButton = document.getElementById("five_button");
+fiveButton.addEventListener("click", () => handleNumberClick('5'));
+const sixButton = document.getElementById("six_button");
+sixButton.addEventListener("click", () => handleNumberClick('6'));
+const sevenButton = document.getElementById("seven_button");
+sevenButton.addEventListener("click", () => handleNumberClick('7'));
+const eightButton = document.getElementById("eight_button");
+eightButton.addEventListener("click", () => handleNumberClick('8'));
+const nineButton = document.getElementById("nine_button");
+nineButton.addEventListener("click", () => handleNumberClick('9'));
 
 
-    // Operators:
+// Operators:
 
-    const addButton = document.getElementById("add_button");
-    addButton.addEventListener("click", () => handleOperatorClick('+'));
-    const subtractButton = document.getElementById("subtract_button");
-    subtractButton.addEventListener("click", () => handleOperatorClick('-'));
-    const multiplyButton = document.getElementById("multiply_button");
-    multiplyButton.addEventListener("click", () => handleOperatorClick('*'));
-    const divideButton = document.getElementById("divide_button");
-    divideButton.addEventListener("click", () => handleOperatorClick('/'));
+const addButton = document.getElementById("add_button");
+addButton.addEventListener("click", () => handleOperatorClick('+'));
+const subtractButton = document.getElementById("subtract_button");
+subtractButton.addEventListener("click", () => handleOperatorClick('-'));
+const multiplyButton = document.getElementById("multiply_button");
+multiplyButton.addEventListener("click", () => handleOperatorClick('*'));
+const divideButton = document.getElementById("divide_button");
+divideButton.addEventListener("click", () => handleOperatorClick('/'));
 
-    // Other buttons:
+// Other buttons:
 
-    const powerButton = document.getElementById("power_button");
-    powerButton.addEventListener("click", function () {
-        // Add function here!
-    });
-    const acButton = document.getElementById("ac_button");
-    acButton.addEventListener("click", function () {
-        clearAll();
-    });
-    const cButton = document.getElementById("c_button");
-    cButton.addEventListener("click", function () {
-        clearLast()
-    });
-    const pointButton = document.getElementById("point_button");
-    pointButton.addEventListener("click", function () {
-        handleDecimalPoint();
-    });
-    const equalsButton = document.getElementById("equals_button");
-    equalsButton.addEventListener("click", function () {
-        updateScreenAnswer();
-    });
+const powerButton = document.getElementById("power_button");
+powerButton.addEventListener("click", function () {
+    // Add function here!
+});
+const acButton = document.getElementById("ac_button");
+acButton.addEventListener("click", function () {
+    clearAll();
+});
+const cButton = document.getElementById("c_button");
+cButton.addEventListener("click", function () {
+    clearLast()
+});
+const pointButton = document.getElementById("point_button");
+pointButton.addEventListener("click", function () {
+    handleDecimalPoint();
+});
+const equalsButton = document.getElementById("equals_button");
+equalsButton.addEventListener("click", function () {
+    updateScreenAnswer();
+});
